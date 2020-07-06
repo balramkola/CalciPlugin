c893ec7d3914dd4ceae1ef1b83e772dd65f40b50
Balram Kola <balramkola@gmail.com>
2020-07-06 19:27:28 +0530

    Added CountDown Timer

--- src/android/Calci.java
+++ src/android/Calci.java
@@ -1,16 +1,32 @@
 package com.balram.myfirstplugin;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import android.util.Log;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.os.CountDownTimer;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Calci extends CordovaPlugin {
+
+    private static final String LOG_TAG = "Calci";
+    private static final String LOG_PREFIX = "PLUGIN: ";
+    private CallbackContext connectionCallbackContext;
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        this.connectionCallbackContext = null;
+        Log.d(LOG_TAG, LOG_PREFIX + "Calci initialized");
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -22,37 +38,103 @@ public class Calci extends CordovaPlugin {
             this.substract(args, callbackContext);
             return true;
         }
+        if (action.equals("startTimer")) {
+            Log.d(LOG_TAG, LOG_PREFIX + "startTimer called");
+            this.connectionCallbackContext = callbackContext;
+            this.startCountDownTimer(200000);
+            return true;
+        }
         return false;
     }
 
     private void add(JSONArray args, CallbackContext callback) {
-       if(args!=null){
-           try{
-               int p1= Integer.parseInt(args.getJSONObject(0).getString("param1"));
-               int p2= Integer.parseInt(args.getJSONObject(0).getString("param2"));
-               callback.success("addition result = "+ (p1+p2));
-           }catch(Exception e){
-            callback.error("Please dont pass null value");
-           }
+        if (args != null) {
+            try {
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.success("addition result = " + (p1 + p2));
+            } catch (Exception e) {
+                callback.error("Please dont pass null value");
+            }
 
-       }else{
-           callback.error("something went wrong");
-       }
+        } else {
+            callback.error("something went wrong");
+        }
     }
 
     private void substract(JSONArray args, CallbackContext callback) {
-        if(args!=null){
-            try{
-                int p1= Integer.parseInt(args.getJSONObject(0).getString("param1"));
-                int p2= Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                callback.success("substract result = "+ (p1-p2));
-            }catch(Exception e){
-             callback.error("Please dont pass null value");
+        if (args != null) {
+            try {
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.success("substract result = " + (p1 - p2));
+            } catch (Exception e) {
+                callback.error("Please dont pass null value");
             }
- 
-        }else{
+
+        } else {
             callback.error("something went wrong");
         }
-     }
+    }
+
+    // Declare timer
+    CountDownTimer cTimer = null;
+
+    // start timer function
+    void startCountDownTimer(int setTimerValue) {
+        stopCountDownTimer();
+        cTimer = new CountDownTimer(setTimerValue, 1000) {
+            public void onTick(long millisUntilFinished) {
+                Log.d(LOG_TAG, LOG_PREFIX + "onTick called");
+                if (Calci.this.webView != null) {
+                    Log.d(LOG_TAG, LOG_PREFIX + "sending update tickSec: " + (millisUntilFinished / 1000));
+                    sendUpdate(getJson("ontick", "" + (millisUntilFinished / 1000)), true);
+                }
+            }
+
+            public void onFinish() {
+                Log.d(LOG_TAG, LOG_PREFIX + "onFinish called");
+                sendUpdate(getJson("onfinish", "Timer Finished"), false);
+                // connectionCallbackContext.success("Timer Finished");
+            }
+        };
+        cTimer.start();
+    }
+
+    // cancel timer
+    void stopCountDownTimer() {
+        if (cTimer != null) {
+            cTimer.cancel();
+            cTimer = null;
+        }
+    }
+
+    /**
+     * Create a new plugin result and send it back to JavaScript
+     */
+    private void sendUpdate(JSONObject info, boolean keepCallback) {
+        if (this.connectionCallbackContext != null) {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, info);
+            result.setKeepCallback(keepCallback);
+            this.connectionCallbackContext.sendPluginResult(result);
+        }
+        // webView.postMessage("networkconnection", type);
+    }
+
+    /**
+     * Creates a JSONObject with the countdown timer information
+     * 
+     * @return a JSONObject containing the timer information
+     */
+    private JSONObject getJson(String eventType, String eventValue) {
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("eventType", eventType);
+            obj.put("eventValue", eventValue);
+        } catch (JSONException e) {
+            Log.d(LOG_TAG, LOG_PREFIX + e.getMessage(), e);
+        }
+        return obj;
+    }
 
 }
--- www/Calci.js
+++ www/Calci.js
@@ -1,9 +1,54 @@
 var exec = require('cordova/exec');
+var cordova = require('cordova');
+var channel = require('cordova/channel');
 
-module.exports.add = function (arg0, success, error) {
+function CountDownTimer() {
+    this.tickSec = 'unknown';
+}
+
+CountDownTimer.prototype.add = function (arg0, success, error) {
     exec(success, error, 'Calci', 'add', [arg0]);
 };
 
-module.exports.substract = function (arg0, success, error) {
+CountDownTimer.prototype.substract = function (arg0, success, error) {
     exec(success, error, 'Calci', 'substract', [arg0]);
 };
+
+CountDownTimer.prototype.startCountDownTimer = function (successCallback, errorCallback) {
+    exec(successCallback, errorCallback, 'Calci', 'startTimer', []);
+};
+
+var countDownTimer = new CountDownTimer();
+
+channel.createSticky('onCordovaConnectionReady');
+channel.waitForInitialization('onCordovaConnectionReady');
+
+channel.onCordovaReady.subscribe(function() {
+    countDownTimer.startCountDownTimer(function(info) {
+        console.log('BRIDGE: successCallback called');
+        if(info.eventType === 'ontick'){
+            countDownTimer.tickSec = info.eventValue;
+            console.log('BRIDGE: firing countdowntimerontick event');
+            cordova.fireDocumentEvent('countdowntimerontick');    
+        }else
+        if(info.eventType === 'onfinish'){
+            console.log('BRIDGE: firing countdowntimeronfinish event');
+            cordova.fireDocumentEvent('countdowntimeronfinish');    
+        }
+       
+        // should only fire this once
+        if (channel.onCordovaConnectionReady.state !== 2) {
+            channel.onCordovaConnectionReady.fire();
+        }
+    },
+    function(e) {
+        // If we can't get the network info we should still tell Cordova
+        // to fire the deviceready event.
+        if (channel.onCordovaConnectionReady.state !== 2) {
+            channel.onCordovaConnectionReady.fire();
+        }
+        console.log('BRIDGE: Error initializing CountDownTimer: ' + e);
+    });
+});
+
+module.exports = countDownTimer;
